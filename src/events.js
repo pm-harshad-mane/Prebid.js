4e0e8f848ec74546631c48c7f464b5f5f9ac5550
Harshad Mane <harshad.mane@pubmatic.com>
2020-10-19 15:20:05 -0700

    modified events.js so that we can import it; also import utils as needed

--- src/events.js
+++ src/events.js
@@ -1,14 +1,15 @@
 /**
  * events.js
  */
-var utils = require('./utils.js');
+// var utils = require('./utils.js');
+import { _map, logMessage, contains, _each, logError, isEmpty } from './utils.js';
 var CONSTANTS = require('./constants.json');
 var slice = Array.prototype.slice;
 var push = Array.prototype.push;
 
 // define entire events
 // var allEvents = ['bidRequested','bidResponse','bidWon','bidTimeout'];
-var allEvents = utils._map(CONSTANTS.EVENTS, function (v) {
+var allEvents = _map(CONSTANTS.EVENTS, function (v) {
   return v;
 });
 
@@ -17,134 +18,139 @@ var idPaths = CONSTANTS.EVENT_ID_PATHS;
 // keep a record of all events fired
 var eventsFired = [];
 
-module.exports = (function () {
-  var _handlers = {};
-  var _public = {};
+// module.exports = (function () {
+var _handlers = {};
+// var publicVar = {};
 
-  /**
+/**
    *
    * @param {String} eventString  The name of the event.
    * @param {Array} args  The payload emitted with the event.
    * @private
    */
-  function _dispatch(eventString, args) {
-    utils.logMessage('Emitting event for: ' + eventString);
+function _dispatch(eventString, args) {
+  logMessage('Emitting event for: ' + eventString);
 
-    var eventPayload = args[0] || {};
-    var idPath = idPaths[eventString];
-    var key = eventPayload[idPath];
-    var event = _handlers[eventString] || { que: [] };
-    var eventKeys = utils._map(event, function (v, k) {
-      return k;
-    });
+  var eventPayload = args[0] || {};
+  var idPath = idPaths[eventString];
+  var key = eventPayload[idPath];
+  var event = _handlers[eventString] || { que: [] };
+  var eventKeys = _map(event, function (v, k) {
+    return k;
+  });
 
-    var callbacks = [];
+  var callbacks = [];
 
-    // record the event:
-    eventsFired.push({
-      eventType: eventString,
-      args: eventPayload,
-      id: key
-    });
+  // record the event:
+  eventsFired.push({
+    eventType: eventString,
+    args: eventPayload,
+    id: key
+  });
 
-    /** Push each specific callback to the `callbacks` array.
+  /** Push each specific callback to the `callbacks` array.
      * If the `event` map has a key that matches the value of the
      * event payload id path, e.g. `eventPayload[idPath]`, then apply
      * each function in the `que` array as an argument to push to the
      * `callbacks` array
      * */
-    if (key && utils.contains(eventKeys, key)) {
-      push.apply(callbacks, event[key].que);
-    }
-
-    /** Push each general callback to the `callbacks` array. */
-    push.apply(callbacks, event.que);
-
-    /** call each of the callbacks */
-    utils._each(callbacks, function (fn) {
-      if (!fn) return;
-      try {
-        fn.apply(null, args);
-      } catch (e) {
-        utils.logError('Error executing handler:', 'events.js', e);
-      }
-    });
+  if (key && contains(eventKeys, key)) {
+    push.apply(callbacks, event[key].que);
   }
 
-  function _checkAvailableEvent(event) {
-    return utils.contains(allEvents, event);
-  }
+  /** Push each general callback to the `callbacks` array. */
+  push.apply(callbacks, event.que);
 
-  _public.on = function (eventString, handler, id) {
-    // check whether available event or not
-    if (_checkAvailableEvent(eventString)) {
-      var event = _handlers[eventString] || { que: [] };
-
-      if (id) {
-        event[id] = event[id] || { que: [] };
-        event[id].que.push(handler);
-      } else {
-        event.que.push(handler);
-      }
-
-      _handlers[eventString] = event;
-    } else {
-      utils.logError('Wrong event name : ' + eventString + ' Valid event names :' + allEvents);
+  /** call each of the callbacks */
+  _each(callbacks, function (fn) {
+    if (!fn) return;
+    try {
+      fn.apply(null, args);
+    } catch (e) {
+      logError('Error executing handler:', 'events.js', e);
     }
-  };
+  });
+}
 
-  _public.emit = function (event) {
-    var args = slice.call(arguments, 1);
-    _dispatch(event, args);
-  };
+export function _checkAvailableEvent(event) {
+  return contains(allEvents, event);
+}
 
-  _public.off = function (eventString, handler, id) {
-    var event = _handlers[eventString];
-
-    if (utils.isEmpty(event) || (utils.isEmpty(event.que) && utils.isEmpty(event[id]))) {
-      return;
-    }
-
-    if (id && (utils.isEmpty(event[id]) || utils.isEmpty(event[id].que))) {
-      return;
-    }
+// publicVar.on = function (eventString, handler, id) {
+export function on(eventString, handler, id) {
+  // check whether available event or not
+  if (_checkAvailableEvent(eventString)) {
+    var event = _handlers[eventString] || { que: [] };
 
     if (id) {
-      utils._each(event[id].que, function (_handler) {
-        var que = event[id].que;
-        if (_handler === handler) {
-          que.splice(que.indexOf(_handler), 1);
-        }
-      });
+      event[id] = event[id] || { que: [] };
+      event[id].que.push(handler);
     } else {
-      utils._each(event.que, function (_handler) {
-        var que = event.que;
-        if (_handler === handler) {
-          que.splice(que.indexOf(_handler), 1);
-        }
-      });
+      event.que.push(handler);
     }
 
     _handlers[eventString] = event;
-  };
+  } else {
+    logError('Wrong event name : ' + eventString + ' Valid event names :' + allEvents);
+  }
+};
 
-  _public.get = function () {
-    return _handlers;
-  };
+// publicVar.emit = function (event) {
+export function emit(event) {
+  var args = slice.call(arguments, 1);
+  _dispatch(event, args);
+};
 
-  /**
+// publicVar.off = function (eventString, handler, id) {
+export function off(eventString, handler, id) {
+  var event = _handlers[eventString];
+
+  if (isEmpty(event) || (isEmpty(event.que) && isEmpty(event[id]))) {
+    return;
+  }
+
+  if (id && (isEmpty(event[id]) || isEmpty(event[id].que))) {
+    return;
+  }
+
+  if (id) {
+    _each(event[id].que, function (_handler) {
+      var que = event[id].que;
+      if (_handler === handler) {
+        que.splice(que.indexOf(_handler), 1);
+      }
+    });
+  } else {
+    _each(event.que, function (_handler) {
+      var que = event.que;
+      if (_handler === handler) {
+        que.splice(que.indexOf(_handler), 1);
+      }
+    });
+  }
+
+  _handlers[eventString] = event;
+};
+
+// publicVar.get = function () {
+export function get() {
+  return _handlers;
+};
+
+/**
    * This method can return a copy of all the events fired
    * @return {Array} array of events fired
    */
-  _public.getEvents = function () {
-    var arrayCopy = [];
-    utils._each(eventsFired, function (value) {
-      var newProp = Object.assign({}, value);
-      arrayCopy.push(newProp);
-    });
+// publicVar.getEvents = function () {
+export function getEvents() {
+  var arrayCopy = [];
+  _each(eventsFired, function (value) {
+    var newProp = Object.assign({}, value);
+    arrayCopy.push(newProp);
+  });
 
-    return arrayCopy;
-  };
+  return arrayCopy;
+};
 
-  return _public;
-}());
+// return publicVar;
+// }());
